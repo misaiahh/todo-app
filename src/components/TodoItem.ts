6d4dfb4139c971503aa465851ed5f91a202dd27d
Micky Hallabrin <misaiahh@proton.me>
2024-06-28 20:07:22 -0400

    updated todo-list to use template string and todo-item to use passed in attribute for textContent

--- src/components/TodoItem.ts
+++ src/components/TodoItem.ts
@@ -1,25 +1,13 @@
 class TodoItem extends HTMLParagraphElement {
-    static observedAttributes = ['textContent'];
-
     constructor() {
         super();
     }
 
     connectedCallback() {
-        console.log("TodoItem added to page.");
-    }
-
-    attributeChangedCallback(name: string, _oldValue: string, _newValue: string) {
-        console.log(`Attribute ${name} has changed from ${_oldValue} to ${_newValue}.`);
-
-        if (name === 'textContent') {
-            this.querySelector('p')!.textContent = _newValue;
-        }
+        this.textContent = this.getAttribute("text-content");
     }
 }
 
 customElements.define('todo-item', TodoItem, {
     extends: 'p'
 });
-
-export default TodoItem;
