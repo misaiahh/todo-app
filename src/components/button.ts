5c44364403c4bfb5ebc67e54649916954ca399ec
Micky Hallabrin <misaiahh@proton.me>
2024-07-20 19:36:31 -0400

    added some additional styles to button component

--- src/components/button.ts
+++ src/components/button.ts
@@ -1,13 +1,28 @@
+const styles = /*html*/`
+    <style>
+    button[is="todo-button"] { 
+        all: unset;
+        cursor: pointer;
+        font-size: 1.2rem;
+        padding: 0.5rem 1rem;
+        background-color: royalblue;
+        color: white;
+        margin: 0.5rem;
+    }
+    </style>
+`;
+
 export default class Button extends HTMLButtonElement {
     constructor() {
         super();
     }
 
     connectedCallback() {
-        this.innerHTML = `${this.getAttribute('text') || 'Click'}`;
+        this.innerHTML = `
+            ${styles}
+            ${this.getAttribute('text') || 'Click'}
+        `;
     }
 }
 
-customElements.define('todo-button', Button, {
-    extends: 'button'
-});
+customElements.define('todo-button', Button, { extends: 'button' });
